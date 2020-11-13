eff770d45cd88a21b7f0462abbb1e87b08167e2a
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-11-13 21:13:24 +0900

    timer calls tokio's timer (not confirmed to work)

--- src/time.rs
+++ src/time.rs
@@ -4,7 +4,12 @@
 //! Time related functionalities.
 pub mod timer {
     //! Timer
-    use futures::{Async, Future, Poll};
+    use futures::{empty as empty01, Async, Empty as Empty01, Future, Poll};
+    use futures03::compat::Future01CompatExt;
+    use futures03::Future as Future03;
+    use futures03::{FutureExt, TryFutureExt};
+    use pin_project::pin_project;
+    use std::pin::Pin;
     use std::sync::mpsc::RecvError;
     use std::time;
 
@@ -15,33 +20,57 @@ pub mod timer {
     pub trait TimerExt: Sized + Future {
         /// Adds the specified timeout to this future.
         fn timeout_after(self, duration: time::Duration) -> TimeoutAfter<Self> {
+            let fut03 = self.compat().timeout_after(duration).map(|x| match x {
+                None => Err(None),
+                Some(Ok(x)) => Ok(x),
+                Some(Err(x)) => Err(Some(x)),
+            });
+            //let fut03 = self.map_err(Some).compat();
             TimeoutAfter {
-                future: self,
-                timeout: timeout(duration),
+                inner: Box::new(fut03.compat()),
             }
         }
     }
     impl<T: Future> TimerExt for T {}
 
+    pub trait TimerExt03: Sized + Future03 {
+        fn timeout_after(self, duration: time::Duration) -> TimeoutAfter03<Self> {
+            let inner = tokio::time::timeout(duration, self);
+            TimeoutAfter03 { inner }
+        }
+    }
+    impl<T: Future03> TimerExt03 for T {}
+
     /// A future which will try executing `T` within the specified time duration.
     ///
     /// If the timeout duration passes, it will return `Err(None)`.
     /// If an error occurres before the expiration time, this will result in `Err(Some(T::Error))`.
-    pub struct TimeoutAfter<T> {
-        future: T,
-        timeout: Timeout,
+    pub struct TimeoutAfter<T: Future + 'static> {
+        inner: Box<dyn Future<Item = T::Item, Error = Option<T::Error>>>,
     }
     impl<T: Future> Future for TimeoutAfter<T> {
         type Item = T::Item;
         type Error = Option<T::Error>;
         fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-            if let Async::Ready(value) = self.future.poll().map_err(Some)? {
-                Ok(Async::Ready(value))
-            } else if let Ok(Async::NotReady) = self.timeout.poll() {
-                Ok(Async::NotReady)
-            } else {
-                Err(None)
-            }
+            self.inner.poll()
+        }
+    }
+
+    #[pin_project]
+    pub struct TimeoutAfter03<T> {
+        #[pin]
+        inner: tokio::time::Timeout<T>,
+    }
+
+    impl<T: Future03> Future03 for TimeoutAfter03<T> {
+        type Output = Option<T::Output>;
+        fn poll(
+            self: std::pin::Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Self::Output> {
+            let this = self.project();
+            let inner: Pin<&mut _> = this.inner;
+            inner.poll(cx).map(|x| x.ok())
         }
     }
 
@@ -50,45 +79,27 @@ pub mod timer {
     /// If this object is dropped before expiration, the timer will be cancelled.
     /// Thus, for example, the repetation of setting and canceling of
     /// a timer only consumpts constant memory region.
-    #[derive(Debug)]
     pub struct Timeout {
-        start: time::Instant,
-        duration: time::Duration,
-        inner: Option<poll::poller::Timeout>,
+        inner: TimeoutAfter<Empty01<(), ()>>,
     }
 
     /// Makes a future which will expire after `delay_from_now`.
     pub fn timeout(delay_from_now: time::Duration) -> Timeout {
         Timeout {
-            start: time::Instant::now(),
-            duration: delay_from_now,
-            inner: None,
+            inner: empty01().timeout_after(delay_from_now),
         }
     }
     impl Future for Timeout {
         type Item = ();
         type Error = RecvError;
         fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-            if let Some(ref mut inner) = self.inner {
-                inner.poll()
-            } else {
-                let duration = self.duration;
-                let elapsed = self.start.elapsed();
-                if elapsed >= duration {
-                    return Ok(Async::Ready(()));
-                }
+            self.inner.poll().map_err(|_| RecvError)
+        }
+    }
 
-                let set_timeout = |mut c: Context| {
-                    let rest = duration - elapsed;
-                    poll::poller::set_timeout(c.poller(), rest)
-                };
-                if let Some(inner) = fiber::with_current_context(set_timeout) {
-                    self.inner = Some(inner);
-                    self.poll()
-                } else {
-                    Ok(Async::NotReady)
-                }
-            }
+    impl std::fmt::Debug for Timeout {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("Timeout").finish()
         }
     }
 
